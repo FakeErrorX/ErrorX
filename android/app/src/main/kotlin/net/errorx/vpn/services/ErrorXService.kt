fa144e0ba942eb8c206341e5463673943c658c32
ErrorX <info.abdullaraju@gmail.com>
2025-04-11 13:19:38 +0600

    fix android

--- android/app/src/main/kotlin/net/errorx/vpn/services/ErrorXService.kt
+++ android/app/src/main/kotlin/net/errorx/vpn/services/ErrorXService.kt
@@ -1,42 +1,51 @@
 package net.errorx.vpn.services
 
+import android.annotation.SuppressLint
+import android.app.Notification.FOREGROUND_SERVICE_IMMEDIATE
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
+import net.errorx.vpn.GlobalState
 import net.errorx.vpn.MainActivity
+import net.errorx.vpn.extensions.getActionPendingIntent
+import net.errorx.vpn.models.VpnOptions
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+
 
 class ErrorXService : Service(), BaseServiceInterface {
+
     private val binder = LocalBinder()
 
     inner class LocalBinder : Binder() {
         fun getService(): ErrorXService = this@ErrorXService
     }
 
-    override fun onBind(intent: Intent?): IBinder {
+    override fun onBind(intent: Intent): IBinder {
         return binder
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return START_NOT_STICKY
+    override fun onUnbind(intent: Intent?): Boolean {
+        return super.onUnbind(intent)
     }
 
     private val CHANNEL = "ErrorX"
 
     private val notificationId: Int = 1
 
-    override fun onCreate() {
-        super.onCreate()
-        startForeground()
-    }
+    private val notificationBuilderDeferred: Deferred<NotificationCompat.Builder> by lazy {
+        CoroutineScope(Dispatchers.Main).async {
+            val stopText = GlobalState.getText("stop")
 
-    private fun startForeground() {
-        try {
             val intent = Intent(
                 this@ErrorXService, MainActivity::class.java
             )
@@ -46,7 +55,7 @@ class ErrorXService : Service(), BaseServiceInterface {
                     this@ErrorXService,
                     0,
                     intent,
-                    PendingIntent.FLAG_IMMUTABLE
+                    PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
                 )
             } else {
                 PendingIntent.getActivity(
@@ -62,23 +71,60 @@ class ErrorXService : Service(), BaseServiceInterface {
                 setContentTitle("ErrorX")
                 setContentIntent(pendingIntent)
                 setCategory(NotificationCompat.CATEGORY_SERVICE)
-                setOngoing(true)
-                val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager?
-                var channel = manager?.getNotificationChannel(CHANNEL)
-                if (channel == null) {
-                    channel =
-                        NotificationChannel(CHANNEL, "ErrorX", NotificationManager.IMPORTANCE_LOW)
-                    manager?.createNotificationChannel(channel)
+                priority = NotificationCompat.PRIORITY_MIN
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    foregroundServiceBehavior = FOREGROUND_SERVICE_IMMEDIATE
                 }
-                startForeground(notificationId, build())
+                addAction(
+                    0,
+                    stopText, // 使用 suspend 函数获取的文本
+                    getActionPendingIntent("STOP")
+                )
+                setOngoing(true)
+                setShowWhen(false)
+                setOnlyAlertOnce(true)
+                setAutoCancel(true)
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        stopForeground(STOP_FOREGROUND_REMOVE)
+    private suspend fun getNotificationBuilder(): NotificationCompat.Builder {
+        return notificationBuilderDeferred.await()
     }
-} 
+
+    override fun start(options: VpnOptions) = 0
+
+    override fun stop() {
+        stopSelf()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            stopForeground(STOP_FOREGROUND_REMOVE)
+        }
+    }
+
+
+    @SuppressLint("ForegroundServiceType")
+    override suspend fun startForeground(title: String, content: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val manager = getSystemService(NotificationManager::class.java)
+            var channel = manager?.getNotificationChannel(CHANNEL)
+            if (channel == null) {
+                channel =
+                    NotificationChannel(CHANNEL, "ErrorX", NotificationManager.IMPORTANCE_LOW)
+                manager?.createNotificationChannel(channel)
+            }
+        }
+        val notification =
+            getNotificationBuilder()
+                .setContentTitle(title)
+                .setContentText(content).build()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            try {
+                startForeground(notificationId, notification, FOREGROUND_SERVICE_TYPE_DATA_SYNC)
+            } catch (_: Exception) {
+                startForeground(notificationId, notification)
+            }
+        } else {
+            startForeground(notificationId, notification)
+        }
+    }
+}
--- android/app/src/main/kotlin/net/errorx/vpn/services/ErrorXVpnService.kt
+++ android/app/src/main/kotlin/net/errorx/vpn/services/ErrorXVpnService.kt
@@ -1,76 +1,130 @@
 package net.errorx.vpn.services
 
+import android.annotation.SuppressLint
+import android.app.Notification.FOREGROUND_SERVICE_IMMEDIATE
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
+import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
+import android.net.ProxyInfo
 import android.net.VpnService
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
-import android.os.ParcelFileDescriptor
+import android.os.Parcel
+import android.os.RemoteException
+import android.util.Log
 import androidx.core.app.NotificationCompat
+import net.errorx.vpn.GlobalState
 import net.errorx.vpn.MainActivity
+import net.errorx.vpn.R
+import net.errorx.vpn.extensions.getActionPendingIntent
+import net.errorx.vpn.extensions.getIpv4RouteAddress
+import net.errorx.vpn.extensions.getIpv6RouteAddress
+import net.errorx.vpn.extensions.toCIDR
+import net.errorx.vpn.models.AccessControlMode
 import net.errorx.vpn.models.VpnOptions
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
 
 class ErrorXVpnService : VpnService(), BaseServiceInterface {
-    private val binder = LocalBinder()
-
-    inner class LocalBinder : Binder() {
-        fun getService(): ErrorXVpnService = this@ErrorXVpnService
+    override fun onCreate() {
+        super.onCreate()
+        GlobalState.initServiceEngine()
     }
 
-    override fun onBind(intent: Intent?): IBinder {
-        return binder
-    }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return START_NOT_STICKY
-    }
-
-    private var vpnInterface: ParcelFileDescriptor? = null
-
-    private var options: VpnOptions? = null
-
-    fun start(options: VpnOptions): Int {
-        this.options = options
-        val builder = Builder()
-            .setMtu(options.mtu)
-            .setSession("ErrorX")
-            .setBlocking(false)
-
-        if (options.ipv6) {
-            builder.addAddress("fdfe:dcba:9876::1", 126)
-            builder.addRoute("::", 0)
-            builder.addDnsServer("fdfe:dcba:9876::2")
-        }
-
-        builder.addAddress("172.19.0.1", 30)
-        builder.addDnsServer("172.19.0.2")
-        builder.addRoute("0.0.0.0", 0)
-
-        if (options.bypassPrivateNetwork) {
-            for (prefix in arrayOf("10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16")) {
-                val parts = prefix.split("/")
-                builder.addDisallowedRoute(parts[0], parts[1].toInt())
+    override fun start(options: VpnOptions): Int {
+        return with(Builder()) {
+            if (options.ipv4Address.isNotEmpty()) {
+                val cidr = options.ipv4Address.toCIDR()
+                addAddress(cidr.address, cidr.prefixLength)
+                val routeAddress = options.getIpv4RouteAddress()
+                if (routeAddress.isNotEmpty()) {
+                    try {
+                        routeAddress.forEach { i ->
+                            Log.d(
+                                "addRoute4",
+                                "address: ${i.address} prefixLength:${i.prefixLength}"
+                            )
+                            addRoute(i.address, i.prefixLength)
+                        }
+                    } catch (_: Exception) {
+                        addRoute("0.0.0.0", 0)
+                    }
+                } else {
+                    addRoute("0.0.0.0", 0)
+                }
             }
-        }
-
-        for (packageName in options.packageNames) {
-            try {
-                builder.addAllowedApplication(packageName)
-            } catch (e: Exception) {
-                e.printStackTrace()
+            if (options.ipv6Address.isNotEmpty()) {
+                val cidr = options.ipv6Address.toCIDR()
+                addAddress(cidr.address, cidr.prefixLength)
+                val routeAddress = options.getIpv6RouteAddress()
+                if (routeAddress.isNotEmpty()) {
+                    try {
+                        routeAddress.forEach { i ->
+                            Log.d(
+                                "addRoute6",
+                                "address: ${i.address} prefixLength:${i.prefixLength}"
+                            )
+                            addRoute(i.address, i.prefixLength)
+                        }
+                    } catch (_: Exception) {
+                        addRoute("::", 0)
+                    }
+                } else {
+                    addRoute("::", 0)
+                }
             }
-        }
+            addDnsServer(options.dnsServerAddress)
+            setMtu(9000)
+            options.accessControl.let { accessControl ->
+                if (accessControl.enable) {
+                    when (accessControl.mode) {
+                        AccessControlMode.acceptSelected -> {
+                            (accessControl.acceptList + packageName).forEach {
+                                addAllowedApplication(it)
+                            }
+                        }
 
-        try {
-            vpnInterface = builder.establish()
-            startForeground()
-            return vpnInterface?.detachFd() ?: -1
-        } catch (e: Exception) {
-            e.printStackTrace()
-            return -1
+                        AccessControlMode.rejectSelected -> {
+                            (accessControl.rejectList - packageName).forEach {
+                                addDisallowedApplication(it)
+                            }
+                        }
+                    }
+                }
+            }
+            setSession("ErrorX")
+            setBlocking(false)
+            if (Build.VERSION.SDK_INT >= 29) {
+                setMetered(false)
+            }
+            if (options.allowBypass) {
+                allowBypass()
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && options.systemProxy) {
+                setHttpProxy(
+                    ProxyInfo.buildDirectProxy(
+                        "127.0.0.1",
+                        options.port,
+                        options.bypassDomain
+                    )
+                )
+            }
+            establish()?.detachFd()
+                ?: throw NullPointerException("Establish VPN rejected by system")
+        }
+    }
+
+    override fun stop() {
+        stopSelf()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            stopForeground(STOP_FOREGROUND_REMOVE)
         }
     }
 
@@ -78,8 +132,9 @@ class ErrorXVpnService : VpnService(), BaseServiceInterface {
 
     private val notificationId: Int = 1
 
-    private fun startForeground() {
-        try {
+    private val notificationBuilderDeferred: Deferred<NotificationCompat.Builder> by lazy {
+        CoroutineScope(Dispatchers.Main).async {
+            val stopText = GlobalState.getText("stop")
             val intent = Intent(this@ErrorXVpnService, MainActivity::class.java)
 
             val pendingIntent = if (Build.VERSION.SDK_INT >= 31) {
@@ -87,7 +142,7 @@ class ErrorXVpnService : VpnService(), BaseServiceInterface {
                     this@ErrorXVpnService,
                     0,
                     intent,
-                    PendingIntent.FLAG_IMMUTABLE
+                    PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
                 )
             } else {
                 PendingIntent.getActivity(
@@ -99,29 +154,93 @@ class ErrorXVpnService : VpnService(), BaseServiceInterface {
             }
 
             with(NotificationCompat.Builder(this@ErrorXVpnService, CHANNEL)) {
-                setSmallIcon(net.errorx.vpn.R.drawable.ic_stat_name)
+                setSmallIcon(R.drawable.ic_stat_name)
                 setContentTitle("ErrorX")
                 setContentIntent(pendingIntent)
                 setCategory(NotificationCompat.CATEGORY_SERVICE)
-                setOngoing(true)
-                val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager?
-                var channel = manager?.getNotificationChannel(CHANNEL)
-                if (channel == null) {
-                    channel =
-                        NotificationChannel(CHANNEL, "ErrorX", NotificationManager.IMPORTANCE_LOW)
-                    manager?.createNotificationChannel(channel)
+                priority = NotificationCompat.PRIORITY_MIN
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    foregroundServiceBehavior = FOREGROUND_SERVICE_IMMEDIATE
                 }
-                startForeground(notificationId, build())
+                setOngoing(true)
+                addAction(
+                    0,
+                    stopText,
+                    getActionPendingIntent("STOP")
+                )
+                setShowWhen(false)
+                setOnlyAlertOnce(true)
+                setAutoCancel(true)
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        stopForeground(STOP_FOREGROUND_REMOVE)
-        vpnInterface?.close()
-        vpnInterface = null
+    private suspend fun getNotificationBuilder(): NotificationCompat.Builder {
+        return notificationBuilderDeferred.await()
     }
-} 
+
+    @SuppressLint("ForegroundServiceType")
+    override suspend fun startForeground(title: String, content: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val manager = getSystemService(NotificationManager::class.java)
+            var channel = manager?.getNotificationChannel(CHANNEL)
+            if (channel == null) {
+                channel =
+                    NotificationChannel(CHANNEL, "ErrorX", NotificationManager.IMPORTANCE_LOW)
+                manager?.createNotificationChannel(channel)
+            }
+        }
+        val notification =
+            getNotificationBuilder()
+                .setContentTitle(title)
+                .setContentText(content)
+                .build()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            try {
+                startForeground(notificationId, notification, FOREGROUND_SERVICE_TYPE_DATA_SYNC)
+            } catch (_: Exception) {
+                startForeground(notificationId, notification)
+            }
+        } else {
+            startForeground(notificationId, notification)
+        }
+    }
+
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
+        GlobalState.getCurrentVPNPlugin()?.requestGc()
+    }
+
+    private val binder = LocalBinder()
+
+    inner class LocalBinder : Binder() {
+        fun getService(): ErrorXVpnService = this@ErrorXVpnService
+
+        override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
+            try {
+                val isSuccess = super.onTransact(code, data, reply, flags)
+                if (!isSuccess) {
+                    CoroutineScope(Dispatchers.Main).launch {
+                        GlobalState.getCurrentTilePlugin()?.handleStop()
+                    }
+                }
+                return isSuccess
+            } catch (e: RemoteException) {
+                throw e
+            }
+        }
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return binder
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        return super.onUnbind(intent)
+    }
+
+    override fun onDestroy() {
+        stop()
+        super.onDestroy()
+    }
+}
